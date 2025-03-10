79dc5584a543b9a65c9ecee72df039a041bb6b2d
llddang <bodyness@naver.com>
2025-03-10 21:24:38 +0900

    feat: layout에 header 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import Header from "@/components/layouts/header";
 
 const pretendard = localFont({
   src: "../public/fonts/PretendardVariable.woff2",
@@ -26,7 +27,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body className={`${pretendard.className} ${riot.variable} antialiased`}>{children}</body>
+      <body className={`${pretendard.className} ${riot.variable} antialiased`}>
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
