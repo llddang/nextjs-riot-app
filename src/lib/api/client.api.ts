fa34863da156e85ff1c8fb827623feb69addd4d2
llddang <bodyness@naver.com>
2025-03-12 20:35:05 +0900

    feat:#12 라우터핸들러의 rotation api 에서 반환하는 값 변경

--- src/lib/api/client.api.ts
+++ src/lib/api/client.api.ts
@@ -1,5 +1,6 @@
 import { API_BASE_URL } from "@/constants/env.constant";
 import { ChampionOverall } from "@/types/champion.type";
+import { Version } from "@/types/common.type";
 
 /**
  * 무료로 사용할 수 있는 챔피언들의 목록을 가져옵니다.
@@ -7,10 +8,12 @@ import { ChampionOverall } from "@/types/champion.type";
  * @returns {Promise<ChampionOverall[]>} 챔피언들의 key 배열
  * @throws {Error} API 요청 실패 시 오류가 발생할 수 있습니다.
  */
-export async function fetchRotationChampions(): Promise<ChampionOverall[]> {
+export async function fetchRotationChampions(): Promise<
+  Version<ChampionOverall[]>
+> {
   const res = await fetch(`${API_BASE_URL}/api/rotation`, {
     cache: "no-store",
   });
-  const data: ChampionOverall[] = await res.json();
+  const data: Version<ChampionOverall[]> = await res.json();
   return data;
 }
