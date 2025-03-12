7c9a78544cf7c1764e35967cc23e53e36c9988c3
llddang <bodyness@naver.com>
2025-03-12 15:13:27 +0900

    refactor:#4 챔피언 카드로 컴포넌트 분리

--- src/app/champions/page.tsx
+++ src/app/champions/page.tsx
@@ -1,3 +1,4 @@
+import ChampionCard from "@/components/features/champion-card";
 import { fetchAllChampionsWithVersion } from "@/lib/api/server.api";
 import Image from "next/image";
 import Link from "next/link";
@@ -12,22 +13,7 @@ export default async function ChampionsPage() {
       </h1>
       <ul className="mt-10 grid grid-cols-[repeat(auto-fit,_minmax(80px,_1fr))] justify-items-center gap-2 md:grid-cols-[repeat(auto-fit,_minmax(90px,_1fr))] md:gap-4">
         {champions.map((champion) => (
-          <li
-            key={champion.id}
-            className="transition-[transform] duration-100 ease-in-out hover:scale-110 hover:shadow-lg hover:shadow-gold"
-          >
-            <Link
-              href={`champions/${champion.id}`}
-              className="relative block h-20 w-20 md:h-24 md:w-24"
-            >
-              <Image
-                src={`https://ddragon.leagueoflegends.com/cdn/15.5.1/img/champion/${champion.image.full}`}
-                fill
-                sizes="20vw"
-                alt={champion.name}
-              />
-            </Link>
-          </li>
+          <ChampionCard key={champion.id} champion={champion} />
         ))}
       </ul>
     </main>
