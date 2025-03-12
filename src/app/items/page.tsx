8a593161928ef45a43513c24aadc050272687b03
llddang <bodyness@naver.com>
2025-03-12 20:18:13 +0900

    refactor:#10 아이템 카드 컴포넌트 분리

--- src/app/items/page.tsx
+++ src/app/items/page.tsx
@@ -1,6 +1,5 @@
+import ItemCard from "@/components/features/item-card";
 import { fetchAllItemsWithVersion } from "@/lib/api/server.api";
-import { removeHtmlTagFromString } from "@/lib/utils/format.util";
-import Image from "next/image";
 
 export default async function ItemsPage() {
   const { version: apiVersion, data: items } = await fetchAllItemsWithVersion();
@@ -12,18 +11,7 @@ export default async function ItemsPage() {
       </h1>
       <ul className="mt-10 grid grid-cols-[repeat(auto-fit,_minmax(5rem,_1fr))] justify-items-center gap-4 md:grid-cols-[repeat(auto-fit,_minmax(6rem,_1fr))]">
         {items.map((item) => (
-          <li key={item.id} className="w-20 rounded-md border-gold md:w-24">
-            <Image
-              src={`https://ddragon.leagueoflegends.com/cdn/${apiVersion}/img/item/${item.image.full}`}
-              alt={item.name}
-              width={96}
-              height={96}
-              className="hover h-20 w-20 border-4 border-gold transition-[transform] duration-100 ease-in-out hover:scale-110 hover:shadow-lg hover:shadow-gold md:h-24 md:w-24"
-            />
-            <span className="break-keep text-sm font-semibold text-gold">
-              {removeHtmlTagFromString(item.name)}
-            </span>
-          </li>
+          <ItemCard item={item} apiVersion={apiVersion} />
         ))}
       </ul>
     </main>
